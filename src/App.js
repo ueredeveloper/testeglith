8bb18a4014d0742334f0db41b5b435a39ad288c7
Glitch (empty-glitch) <none>
2020-06-04 15:30:07 +0000

    👛🍮 Checkpoint ./src/App.js:11103236/13

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class App extends Component {
   render (){
     return (
-      <p>Olá Mundo</p>
+      <p>Olá Mundo Git e Glitch</p>
     )
   }
 }
