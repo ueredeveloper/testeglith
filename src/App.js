9dae65f17f837169c52592cb62090450da7d8ccf
Fabrício José Barrozo <ueredeveloper@gmail.com>
2020-06-05 18:18:24 -0300

    commit com vs code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <p>Olá Mundo Glitch 2</p>
+        <p>Olá Mundo Vs Code 3</p>
       </div>
     );
   }
