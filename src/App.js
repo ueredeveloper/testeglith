280c662a60856c6f2054f48477fd690e9c818bcb
Fabrício José Barrozo <ueredeveloper@gmail.com>
2020-06-05 16:50:27 -0300

    quinto update vs code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class App extends Component {
   render (){
     return (
-      <p>Olá Mundo Git e Glitch - Quarto Update - VsCode</p>
+      <p>Olá Mundo Git e Glitch - Quinto Update - VsCode</p>
     )
   }
 }
