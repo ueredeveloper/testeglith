bd54dc3259e31142d9a6dd57215605fd6dab051d
Glitch (empty-glitch) <none>
2020-06-06 23:00:00 +0000

    🏍🦁 Checkpoint ./src/Draggable.js:11103236/4991

--- src/Draggable.js
+++ src/Draggable.js
@@ -1,13 +1,46 @@
 import React from "react";
 import "./style.css";
 
+var pos1 = 0,
+  pos2 = 0,
+  pos3 = 0,
+  pos4 = 0;
+
 const Draggable = () => {
+  var eleDiv = React.createElement("div", {id:'mydivheader'}, "Clique Here");
+
+  const closeDragElement = () => {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  };
+
+  const dragMouseDown = e => {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = this.closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = this.elementDrag;
+  };
   
-  
-  var eleDiv = React.createElement(, {toWhat: 'World'}, null)
-  
-  
-  return <div>Olá Div Function</div>;
+   const elementDrag = (e) => {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+
+  return <div>{eleDiv}</div>;
 };
 
 export default Draggable;
