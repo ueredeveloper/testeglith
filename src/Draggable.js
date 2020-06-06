b252e76521f12f0b54a1dcfccf218ff29aae72db
Glitch (empty-glitch) <none>
2020-06-06 22:13:01 +0000

    🍣🔖 Checkpoint ./src/Draggable.js:11103236/4288

--- src/Draggable.js
+++ src/Draggable.js
@@ -2,30 +2,33 @@ import React, { Component } from "react";
 import "./style.css";
 // link http://jsfiddle.net/Af9Jt/2/
 
-const divElement = (
-  <div>
-    <div id="mydivheader">Here</div>
-  </div>
-);
+var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
 export default class Draggable extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      pos1: null,
-      pos2: null,
-      pos3: null,
-      pos4: null
-    };
+  
   }
+   divElement = props => {
+    return  <div id="mydivheader">Here</div>;
+  };
+ 
+  const divElement = (props) => {
+  return 
+};
+    
+    function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
 
   elementDrag = e => {
     e = e || window.event;
     e.preventDefault();
     // calculate the new cursor position:
-    this.pos1 = this.pos3 - e.clientX;
-    this.pos2 = this.pos4 - e.clientY;
-    this.pos3 = e.clientX;
-    this.pos4 = e.clientY;
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
     // set the element's new position:
     // elmnt.style.top = elmnt.offsetTop - pos2 + "px";
     //elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
