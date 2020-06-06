9eb094e783a00f7eb080b228c6ad06cc5b589710
Glitch (empty-glitch) <none>
2020-06-06 22:48:02 +0000

    🍡🌘 Checkpoint ./src/Draggable.js:11103236/4599

--- src/Draggable.js
+++ src/Draggable.js
@@ -1,46 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style.css";
-// link http://jsfiddle.net/Af9Jt/2/
 
-var pos1 = 0,
-  pos2 = 0,
-  pos3 = 0,
-  pos4 = 0;
+const Draggable = () => {
+  
+  
+  var eleDiv = React.createElement(, {toWhat: 'World'}, null)
+  
+  
+  return <div>Olá Div Function</div>;
+};
 
-export default class Draggable extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  elemDrag = e => {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-
-    let button = document.getElementById("mydivheader");
-
-    // set the element's new position:
-    button.style.top = button.offsetTop - pos2 + "px";
-    button.style.left = button.offsetLeft - pos1 + "px";
-
-    console.log(button);
-  };
-
-  render() {
-    return (
-      <div
-        style={{
-          backgroundColor: "red"
-        }}
-      >
-        <div id="mydivheader" onClick={this.elemDrag}>
-          Draggable
-        </div>
-      </div>
-    );
-  }
-}
+export default Draggable;
