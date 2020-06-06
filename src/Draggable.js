0b5763e0b1bbda68e2c4039679baf46c881eaa85
Glitch (empty-glitch) <none>
2020-06-06 22:24:55 +0000

    🏠🎡 Checkpoint ./src/Draggable.js:11103236/4088

--- src/Draggable.js
+++ src/Draggable.js
@@ -2,26 +2,17 @@ import React, { Component } from "react";
 import "./style.css";
 // link http://jsfiddle.net/Af9Jt/2/
 
-var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+var pos1 = 0,
+  pos2 = 0,
+  pos3 = 0,
+  pos4 = 0;
 
 export default class Draggable extends Component {
   constructor(props) {
     super(props);
-  
   }
-   divElement = props => {
-    return  <div id="mydivheader">Here</div>;
-  };
- 
-  const divElement = (props) => {
-  return 
-};
-    
-    function GuestGreeting(props) {
-  return <h1>Please sign up.</h1>;
-}
 
-  elementDrag = e => {
+  elemDrag = e => {
     e = e || window.event;
     e.preventDefault();
     // calculate the new cursor position:
@@ -29,15 +20,20 @@ export default class Draggable extends Component {
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
+    
+    let button = document.getElementById('mydivheader')
+
     // set the element's new position:
-    // elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    //elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    button.style.top = button.offsetTop - pos2 + "px";
+    button.style.left = button.offsetLeft - pos1 + "px";
   };
 
   render() {
     return (
       <div>
-        <divElement></divElement>
+        <div id="mydivheader" onClick={this.elemDrag}>
+          Draggable
+        </div>
       </div>
     );
   }
