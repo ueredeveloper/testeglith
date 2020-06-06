046875230ad3744bf56ac13c97031141c5a2e3f0
Glitch (empty-glitch) <none>
2020-06-06 20:02:55 +0000

    🐫🏅 Checkpoint ./src/App.js:11103236/1565

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 
+  const button = {
+    position: 'absolute',
+    left: '100px',
+    top: '150px',
+  }
+  
+  const btnInput = ()=> {
+    return <button id="btn" style={button} onClick={this.setX()}>.</button>
+      
+      
+  }
+  
 export default class App extends Component {
+  
+
+
+  setX = () => {
+    var rect = bntInput.getBoundingClientRect();
+    console.log(rect.top, rect.right, rect.bottom, rect.left);
+  };
+
   render() {
     return (
       <div>
-        <p>Olá Mundo VS code 6</p>
+        <div>
+          <input></input>
+          
+        </div>
       </div>
     );
   }
