cc3916ea875584cb9d3e5944803658c711d10638
Glitch (empty-glitch) <none>
2020-06-06 21:00:56 +0000

    🐘⭐️ Checkpoint ./src/Draggable.js:11103236/480

--- src/Draggable.js
+++ src/Draggable.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+// link http://jsfiddle.net/Af9Jt/2/
+
 export default class Draggable extends Component {
   constructor(props) {
     super(props);
@@ -7,8 +9,26 @@ export default class Draggable extends Component {
       initialProps: { x: 0, y: 0 }
     };
   }
+  
+  
+  onMouseDown = (e) => {
+    // only left mouse button
+    if (e.button !== 0) return
+    var pos = (this.getDOMNode()).offset()
+    this.setState({
+      dragging: true,
+      rel: {
+        x: e.pageX - pos.left,
+        y: e.pageY - pos.top
+      }
+    })
+    e.stopPropagation()
+    e.preventDefault()
+    
+    console.log(pos)
+  }
 
   render() {
-    return <div>Dragable</div>;
+    return <div><button onClick={this.onMouseDown}></button></div>;
   }
 }
