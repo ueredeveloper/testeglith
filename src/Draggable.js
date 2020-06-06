457aaf96cc2462d42bad1c8a7f85a0cf21cbaef4
Glitch (empty-glitch) <none>
2020-06-06 21:12:53 +0000

    👲👩‍❤️‍💋‍👩 Checkpoint ./src/Draggable.js:11103236/647 ./src/style.css:11103236/839 ./src/App.js:11103236/48

--- src/Draggable.js
+++ src/Draggable.js
@@ -1,34 +1,15 @@
 import React, { Component } from "react";
-
+import './style.css';
 // link http://jsfiddle.net/Af9Jt/2/
 
 export default class Draggable extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      initialProps: { x: 0, y: 0 }
-    };
-  }
-  
-  
-  onMouseDown = (e) => {
-    // only left mouse button
-    if (e.button !== 0) return
-    var pos = (this.getDOMNode()).offset()
-    this.setState({
-      dragging: true,
-      rel: {
-        x: e.pageX - pos.left,
-        y: e.pageY - pos.top
-      }
-    })
-    e.stopPropagation()
-    e.preventDefault()
-    
-    console.log(pos)
-  }
 
+  }
+  
+  
   render() {
-    return <div><button onClick={this.onMouseDown}></button></div>;
+    return <div id="mydivheader">Here</div>;
   }
 }
