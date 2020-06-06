f59ba3f30078f5dce8f522c56d1c3a5e62e8ccaf
Glitch (empty-glitch) <none>
2020-06-06 21:36:56 +0000

    👮🌪 Checkpoint ./src/Draggable.js:11103236/1693

--- src/Draggable.js
+++ src/Draggable.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
-import './style.css';
+import "./style.css";
 // link http://jsfiddle.net/Af9Jt/2/
 
 export default class Draggable extends Component {
   constructor(props) {
     super(props);
-
   }
-  
-  
+
+  elementDrag = e => {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  };
+
   render() {
     return <div id="mydivheader">Here</div>;
   }
