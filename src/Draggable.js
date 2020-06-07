a50daa6a338e1e85351252171b3e4125069c2e41
Glitch (empty-glitch) <none>
2020-06-07 20:43:13 +0000

    🎅🎐 Checkpoint ./src/Draggable.js:11103236/7187 ./public/index.html:11103236/3009

--- src/Draggable.js
+++ src/Draggable.js
@@ -5,7 +5,8 @@ import MenuItem from "@material-ui/core/MenuItem";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import ControlCameraIcon from "@material-ui/icons/ControlCamera";
-
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 import "./style.css";
 
 var pos1 = 0,
@@ -14,11 +15,11 @@ var pos1 = 0,
   pos4 = 0;
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch"
-    }
+  margin: {
+    margin: theme.spacing(1)
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1)
   }
 }));
 
@@ -74,11 +75,12 @@ const Draggable = () => {
       <div>
         <form className={classes.root} noValidate autoComplete="off">
           <TextField id="standard-basic" label="Standard" />
-
-          <IconButton size="small">
+          <IconButton className={classes.margin} size="small">
             <ControlCameraIcon fontSize="small" />
           </IconButton>
-
+          <IconButton aria-label="delete" className={classes.margin}>
+            <DeleteIcon fontSize="small" />
+          </IconButton>
           <Button
             aria-controls="simple-menu"
             aria-haspopup="true"
