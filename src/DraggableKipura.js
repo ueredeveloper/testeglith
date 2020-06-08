4f98583a75fa949d3d3d8f7f776a55f6238eec46
Glitch (empty-glitch) <none>
2020-06-08 14:46:21 +0000

    ⛽️💴 Checkpoint ./src/DraggableKipura.js:11103236/6616

--- src/DraggableKipura.js
+++ src/DraggableKipura.js
@@ -3,13 +3,68 @@ import React, { useEffect, useState, useRef } from "react";
 import "./styleDK.css";
 
 const DraggableKipura = props => {
+  const divRef = useRef(null);
+  var active = false;
+  var currentX;
+  var currentY;
+  var initialX;
+  var initialY;
+  var xOffset = 0;
+  var yOffset = 0;
   
-   const divRef = useRef(null);
+  const [dra]
+
+  useEffect(() => {
+    let dragItem  = divRef.current;
+
+  }, []);
   
-   let div = divRef.current;
   
-  console.log(div)
-  
+   function dragStart(e) {
+      if (e.type === "touchstart") {
+        initialX = e.touches[0].clientX - xOffset;
+        initialY = e.touches[0].clientY - yOffset;
+      } else {
+        initialX = e.clientX - xOffset;
+        initialY = e.clientY - yOffset;
+      }
+
+      if (e.target === dragItem) {
+        active = true;
+      }
+    }
+
+    function dragEnd(e) {
+      initialX = currentX;
+      initialY = currentY;
+
+      active = false;
+    }
+
+    function drag(e) {
+      if (active) {
+      
+        e.preventDefault();
+      
+        if (e.type === "touchmove") {
+          currentX = e.touches[0].clientX - initialX;
+          currentY = e.touches[0].clientY - initialY;
+        } else {
+          currentX = e.clientX - initialX;
+          currentY = e.clientY - initialY;
+        }
+
+        xOffset = currentX;
+        yOffset = currentY;
+
+        setTranslate(currentX, currentY, dragItem);
+      }
+    }
+
+    function setTranslate(xPos, yPos, el) {
+      el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+    }
+
   return (
     <div id="outerContainer" ref={divRef}>
       <div id="container">
