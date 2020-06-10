f7474d953afca44dc58214489e95f789dbf4eb6e
Glitch (empty-glitch) <none>
2020-06-10 04:24:04 +0000

    ⛵️🕴 Checkpoint ./src/App.js:11103236/2600

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import InputDraggable from "./InputDraggable";
 
-
 const App = () => {
   const [data, setData] = useState({
     ideas: [
       {
+        id: 1,
         content: "Loop For",
         style: {
           top: "50px",
@@ -13,6 +13,7 @@ const App = () => {
         }
       },
       {
+        id: 2,
         content: "Switch",
         style: {
           top: "180px",
@@ -21,33 +22,32 @@ const App = () => {
       }
     ]
   });
-  
-  const addNewIdea=(idea)=>{
-    console.log('App add new  Idea')
-    
-     setData({
-        ...data,
-        ideas: [...data.ideas, idea]
-      });
-  }
-  
-    const deleteIdea =(id)=>{
-    console.log('App deleteIdea')
-    
-     setData({
-        ...data,
-        ideas: [...data.ideas, idea]
-      });
-  }
-    
+
+  const addNewIdea = idea => {
+    console.log("App add new  Idea");
+
+    setData({
+      ...data,
+      ideas: [...data.ideas, idea]
+    });
+  };
+
+  const deleteIdea = id => {
+    console.log("App deleteIdea");
+
+    for (var i = 0; i < data.ideas.length; i++) {
+      if (data.ideas[i].id === 1) {
+        data.ideas.splice(i, 1);
+      }
+    }
+  };
 
   return (
     <div>
       <div>Olá Mundo Brasil</div>
       {data.ideas.map((idea, i) => (
         <div>
-          <InputDraggable idea={idea} key={i} addNewIdea={addNewIdea} />
-         
+          <InputDraggable idea={idea} key={i} addNewIdea={addNewIdea} deleteIdea={deleteIdea(id)} />
         </div>
       ))}
     </div>
