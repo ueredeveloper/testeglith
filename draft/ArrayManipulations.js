3b4cf8d3c6f0585808b0c106bc6185f5a091dd97
Glitch (empty-glitch) <none>
2020-06-10 16:51:10 +0000

    🙏🏡 Checkpoint ./.gitignore:11103236/11

---
+++ draft/ArrayManipulations.js
@@ -0,0 +1,16 @@
+   /*
+    let ideas = data.ideas;
+
+    console.log(ideas);
+    for (var i = 0; i < ideas.length; i++) {
+      if (ideas[i].id === id) {
+        ideas.splice(i, 1);
+      }
+    }
+
+    setData({
+      ...data,
+      ideas: ideas
+    });
+    */
+    
