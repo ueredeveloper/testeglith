3bd46bbcd2062221035bd0cba5c16f59fb18fe3f
Glitch (empty-glitch) <none>
2020-06-10 17:16:23 +0000

    🚊🎰 Checkpoint ./src/InputDraggKipura.js:11103236/6112

--- src/InputDraggKipura.js
+++ src/InputDraggKipura.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useLayoutEffect } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
@@ -6,63 +6,39 @@ import ControlCameraIcon from "@material-ui/icons/ControlCamera";
 import IconButton from "@material-ui/core/IconButton";
 
 import "./styleKipura.css";
-import InputMenu from "./InputMenu";
 
-var initialX;
-var initialY;
+var active = false;
 var currentX;
 var currentY;
-
+var initialX;
+var initialY;
 var xOffset = 0;
 var yOffset = 0;
 
-const useStyles = makeStyles(theme => ({
-  margin: {
-    margin: theme.spacing(1)
-  },
-  extendedIcon: {
-    marginRight: theme.spacing(1)
-  }
-}));
+var container;
 
-const InputDraggable = props => {
+const InputDraggKipura = props => {
   const divRef = useRef(null);
 
- 
-  const dragTouchStart = e => {
-    initialX = e.touches[0].clientX - xOffset;
-    initialY = e.touches[0].clientY - yOffset;
+  const longResolve = () => {
+    return new Promise(res => {
+      setTimeout(res, 3000);
+    });
   };
 
-  const dragTouchMove = e => {
-    e.preventDefault();
-
-    currentX = e.touches[0].clientX - initialX;
-    currentY = e.touches[0].clientY - initialY;
-
-    xOffset = currentX;
-    yOffset = currentY;
-
-    let div = divRef.current;
-
-    div.style.transform =
-      "translate3d(" + currentX + "px, " + currentY + "px, 0)";
-
-    // set the element's new position:
-    // div.style.top = currentX + "px";
-    // div.style.left = currentX + "px";
-  };
-
-  const dragTouchEnd = e => {
-    initialX = currentX;
-    initialY = currentY;
-  };
+  useEffect(() => {
+    longResolve().then(() => {
+      container = divRef.current;
+     
+    });
+  }, []);
 
   return (
     <div id="container">
+      { console.log(container)}
       <div id="item"></div>
     </div>
   );
 };
 
-export default InputDraggable;
+export default InputDraggKipura;
