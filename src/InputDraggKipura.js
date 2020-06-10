3a062615edb7a58004932cbb6ae9e4c74be02945
Glitch (empty-glitch) <none>
2020-06-10 17:28:23 +0000

    🐝🎾 Checkpoint ./src/InputDraggKipura.js:11103236/1530

--- src/InputDraggKipura.js
+++ src/InputDraggKipura.js
@@ -15,10 +15,11 @@ var initialY;
 var xOffset = 0;
 var yOffset = 0;
 
-var container;
+var dragItem, container;
 
 const InputDraggKipura = props => {
-  const divRef = useRef(null);
+  const divDragItem = useRef(null);
+    const divContainer = useRef(null);
 
   const longResolve = () => {
     return new Promise(res => {
@@ -28,15 +29,62 @@ const InputDraggKipura = props => {
 
   useEffect(() => {
     longResolve().then(() => {
-      container = divRef.current;
+      dragItem = divDragItem.current;
+      container = divContainer.current;
      
     });
   }, []);
+  
+  
+   const dragStart = e => {
+        if (e.type === "touchstart") {
+          initialX = e.touches[0].clientX - xOffset;
+          initialY = e.touches[0].clientY - yOffset;
+        } else {
+          initialX = e.clientX - xOffset;
+          initialY = e.clientY - yOffset;
+        }
+
+        if (e.target === dragItem) {
+          active = true;
+        }
+      }
+
+      const dragEnd = e => {
+        initialX = currentX;
+        initialY = currentY;
+
+        active = false;
+      }
+
+      const drag = e => {
+        if (active) {
+          e.preventDefault();
+
+          if (e.type === "touchmove") {
+            currentX = e.touches[0].clientX - initialX;
+            currentY = e.touches[0].clientY - initialY;
+          } else {
+            currentX = e.clientX - initialX;
+            currentY = e.clientY - initialY;
+          }
+
+          xOffset = currentX;
+          yOffset = currentY;
+
+          setTranslate(currentX, currentY, dragItem);
+        }
+      }
+
+      const setTranslate = (xPos, yPos, el) => {
+        el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+      }
+  
 
   return (
-    <div id="container">
-      { console.log(container)}
-      <div id="item"></div>
+    <div id="container" ref={divContainer}>
+      { }
+      <div id="item" ref={divDragItem}></div>
     </div>
   );
 };
