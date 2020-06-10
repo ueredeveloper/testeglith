e9ecce786afa9f1eab552337bba55f8617f4b62d
Glitch (empty-glitch) <none>
2020-06-10 21:43:21 +0000

    👨‍👨‍👦🧀 Checkpoint ./src/teste/InputDraggable.js:11103236/9430 ./src/App.js:11103236/257

---
+++ src/teste/InputDraggable.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef } from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import ControlCameraIcon from "@material-ui/icons/ControlCamera";
+import IconButton from "@material-ui/core/IconButton";
+import InputMenu from "./InputMenu";
+
+const InputDraggable = props => {
+  return <div>Olá mUndo</div>;
+};
+
+export default InputDraggable;
