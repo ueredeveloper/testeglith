1a805d268bab3f4c9b7f0dbc0f3615d5f5e8beea
Glitch (empty-glitch) <none>
2020-06-12 03:53:52 +0000

    🏓🏀 Checkpoint ./src/teste/InputDraggable.js:11103236/6337 ./src/App.js:11103236/4 ./src/teste/style.css:11103236/679

--- src/teste/InputDraggable.js
+++ src/teste/InputDraggable.js
@@ -1,115 +1,135 @@
 import React, { useEffect, useRef } from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import ControlCameraIcon from "@material-ui/icons/ControlCamera";
+import IconButton from "@material-ui/core/IconButton";
+//import InputMenu from "./InputMenu";
+
 import "./style.css";
 
-var active = false;
-var currentX;
-var currentY;
-var initialX;
-var initialY;
-var xOffset = 0;
-var yOffset = 0;
-
-var dragItem, container;
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1)
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1)
+  }
+}));
 
 const InputDraggable = props => {
-  const divDragItem = useRef(null);
-  const divContainer = useRef(null);
+  const divRef = useRef(null);
+  var container;
+
+  var initialX, initialY, currentX, currentY;
 
   useEffect(() => {
-    dragItem = divDragItem.current;
-    container = divContainer.current;
-  }, []);
+    container = divRef.current;
+  });
+
+  const onMouseDown = e => {
+    e = e || window.event;
+   // e.preventDefault();
+    // get the mouse cursor position at startup:
+    currentX = e.clientX;
+    currentY = e.clientY;
+    document.onmouseup = onMouseUp;
+    document.onmousemove = onMouseMove;
+  };
+
+  const onMouseMove = e => {
+    e = e || window.event;
+    e.preventDefault();
+    e.stopPropagation();
+
+    initialX = currentX - e.clientX;
+    initialY = currentY - e.clientY;
+    currentX = e.clientX;
+    currentY = e.clientY;
+
+    container.style.top = container.offsetTop - initialY + "px";
+    container.style.left = container.offsetLeft - initialX + "px";
+  };
+
+  const onMouseUp = () => {
+    document.onmouseup = null;
+    document.onmousemove = null;
+
+    props.idea.style.top = container.offsetTop - initialY;
+    props.idea.style.left = container.offsetLeft - initialX;
+    props.idea.style.width = container.offsetWidth;
+    props.idea.style.height = container.offsetHeight;
+  };
 
   const onTouchStart = e => {
-    console.log("on touch start");
+    e = e || window.event;
 
-    initialX = e.touches[0].clientX - xOffset;
-    initialY = e.touches[0].clientY - yOffset;
-
-    dragItem.addEventListener("touchmove", onTouchMove, false);
-    
-    dragItem.addEventListener("mousedown", onMouseDown, false);
-    dragItem.addEventListener("touchend", onTouchEnd, false);
-
-
-    // dragItem.ontouchend = onTouchEnd;
-    // dragItem.ontouchmove = onTouchMove;
-    // dragItem.onmousedown = onMouseDown;
-
-    if (e.target === dragItem) {
-      active = true;
-    }
+    currentX = e.touches[0].clientX;
+    currentY = e.touches[0].clientY;
+    document.ontouchend = onTouchEnd;
+    document.ontouchmove = onTouchMove;
   };
 
   const onTouchMove = e => {
-    console.log("touch move");
-    if (active) {
-      e.preventDefault();
+    e = e || window.event;
+    e.preventDefault();
+    e.stopPropagation();
 
-      currentX = e.touches[0].clientX - initialX;
-      currentY = e.touches[0].clientY - initialY;
-    }
+    initialX = currentX - e.touches[0].clientX;
+    initialY = currentY - e.touches[0].clientY;
+    currentX = e.touches[0].clientX;
+    currentY = e.touches[0].clientY;
 
-    xOffset = currentX;
-    yOffset = currentY;
-
-    setTranslate(currentX, currentY, dragItem);
+    container.style.top = container.offsetTop - initialY + "px";
+    container.style.left = container.offsetLeft - initialX + "px";
   };
 
   const onTouchEnd = e => {
-    console.log("touch end");
-    initialX = currentX;
-    initialY = currentY;
- 
-    active = false;
-    /*
-    dragItem.ontouchend = null;
-    dragItem.ontouchmove = null;
-    dragItem.onmousedown = null;
-    dragItem.ontouchstart = null;
-    */
+    document.ontouchend = null;
+    document.ontouchmove = null;
 
-    /*
-    dragItem.removeEventListener("touchend", onTouchEnd);
-    dragItem.removeEventListener("touchmove", onTouchEnd);
-    dragItem.removeEventListener("mousedown", onMouseDown);
-    */
+    props.idea.style.top = container.offsetTop - initialY;
+    props.idea.style.left = container.offsetLeft - initialX;
+    props.idea.style.width = container.offsetWidth;
+    props.idea.style.height = container.offsetHeight;
   };
 
-  const onMouseDown = e => {
-    console.log("on mouse down");
-    initialX = e.clientX - xOffset;
-    initialY = e.clientY - yOffset;
-
-    if (e.target === dragItem) {
-      active = true;
-    }
+  const classes = useStyles();
+  const [value, setValue] = React.useState(props.idea.content);
+  const handleChange = event => {
+    setValue(event.target.value);
   };
-
-  const setTranslate = (xPos, yPos, el) => {
-    console.log("translate");
-    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-  };
-
   return (
-    <div>
-      {props.data.ideas.map((idea, i) => (
-        <div
-          id="item"
-          ref={divDragItem}
-          onTouchStart={onTouchStart}
-          key={i}
-        ></div>
-      ))}
+    <div
+      id="container"
+      style={{
+        top: props.idea.style.top + "px",
+        left: props.idea.style.left + "px"
+      }}
+      ref={divRef}
+    >
+      <div>
+        <form className={classes.root} noValidate autoComplete="off">
+          <IconButton className={classes.margin} size="small">
+            <ControlCameraIcon
+              onMouseDown={onMouseDown}
+              onTouchStart={onTouchStart}
+              fontSize="small"
+            />
+          </IconButton>
+          <TextField
+            id="standard-textarea"
+            label="Multiline Placeholder"
+            placeholder="Placeholder"
+            multiline
+            value={value}
+            onChange={handleChange}
+          />
+        </form>
+        
+      </div>
     </div>
   );
 };
 
 export default InputDraggable;
-
-/*
-
- onTouchEnd={onTouchEnd}
-          onTouchMove={onTouchMove}
-          onMouseDown={onMouseDown}  */
